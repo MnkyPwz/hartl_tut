1758f87ed10ddb898b568aba96043feed0bec817
MnkyPwz <odoms@me.com>
2012-10-28 18:47:00 -0500

    Improved Readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HartTut::Application.config.secret_token = 'bd0acab993f1eae892ad9deca8783c2d07fc59ce754292eb5f3a2b7814b1b086ae4e14a3ed41a726a8fe01d0eaf791a64aaa4f958dd1bb2a9067181577c85519'
